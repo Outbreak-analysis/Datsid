1b313fa9d4d208d12becf0c63264d9277e0aa0e3
davidchampredon <david.champredon@gmail.com>
2016-03-17 13:28:49 -0400

    minor edits

--- plot_data.R
+++ plot_data.R
@@ -24,9 +24,9 @@ q.all$reportdate <- as.Date(q.all$reportdate)
 q.all$fullloc <- paste(q.all$country,q.all$adminDiv1,q.all$adminDiv2)
 
 ## Plots
-pdf(paste0("plot_data_",db.name,".pdf"),width=15,height=10)
+pdf(paste0("plot_data_",db.name,".pdf"),width=35,height=20)
 g <- ggplot(q.all)+geom_step(aes(x=reportdate,y=count,colour=eventtype),size=1)
 g <- g + ggtitle("Epidemics in database")
-g <- g +facet_wrap(~fullloc+disease_name,scales = "free")
+g <- g + facet_wrap(~fullloc+disease_name,scales = "free",ncol = 4)
 plot(g)
 dev.off()
