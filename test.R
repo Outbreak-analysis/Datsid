db98653de07df84aff4086dc1e141a94db2a2ca8
davidchampredon <david.champredon@gmail.com>
2016-03-23 18:21:07 -0400

    improve test script

--- test.R
+++ test.R
@@ -1,8 +1,17 @@
 source("read_DB.R")
 
+# Build a new database:
+db.name <- "test.db"
+system(paste("./buildNewDB",db.name))
 
-db.path <- "a.db"
-country <- "UK"
-disease <- "measles"
-x <- get.epi.ts(db.path,country,disease)
-head(x)
+# Pick an epidemic:
+country <- "RDC"
+disease <- "ebola"
+
+# Retrieve the data:
+x <- get.epi.ts(db.name, country, disease, synthetic = NULL)
+
+# Plot the data:
+library(ggplot2)
+x$date <- as.Date(x$reportdate)
+ggplot(x) + geom_line(aes(x=date,y=count))
