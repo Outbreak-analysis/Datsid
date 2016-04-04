efa9a3a9035f6b082fe4963b1d2f326479ddd9ba
davidchampredon <david.champredon@gmail.com>
2016-04-03 21:20:05 -0400

    fct for slicing recurrent epidemics

--- utils.R
+++ utils.R
@@ -21,3 +21,43 @@ peaks <- function(x,w,do.plot=FALSE){
 troughs <- function(x,w,do.plot=FALSE){
 	return(peaks(-x,w,do.plot))
 }
+
+
+slice.recurrent.timeseries <- function(dat,w){
+	### SLiCES A TIME SERIES WITH RECURRING EPIDEMICS
+	###
+	
+	pk <- peaks(x = dat$inc, w)
+	tr <- troughs(x = dat$inc, w)
+	
+	t.pk <- dat$t[pk]
+	t.tr <- dat$t[tr]
+	t.tr <- t.tr[diff(t.tr)>w]
+	if(t.tr[1]<t.pk[1]) t.tr <- t.tr[1:length(t.pk)]
+	if(t.tr[1]>t.pk[1]) {
+		while(length(t.tr)!=length(t.pk)) t.pk <- t.pk[-1]
+	}
+	inc.tr <- dat$inc[dat$t %in% t.tr]
+	
+	plot(dat$t,dat$inc,typ='l')
+	points(t.pk,dat$inc[pk], col='red',pch=16)
+	points(t.tr,inc.tr, col='blue',pch=1)
+	text(x=t.tr,y=inc.tr, labels = t.tr,pos=1,cex=0.5)
+	
+	n.pk <- sum(pk)
+	n.tr <- sum(tr)
+	n <- min(c(length(t.tr),length(t.pk)))
+	slice <- list()
+	
+	for(i in 1:n){ 
+		tslice <- t.tr[i]:t.pk[i]
+		inc_i <-  dat$inc[dat$t %in% tslice]
+		slice[[i]] <- data.frame(t = 1:length(inc_i), 
+								 inc = inc_i)
+	}
+	
+	# for(i in 1:length(slice)){ 
+	# 	plot(slice[[i]]$t, slice[[i]]$inc,typ='o',log='y')
+	# }
+	return(slice)
+}
