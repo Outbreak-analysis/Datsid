2ce0f17662ef162676131050f8a09d465ceba68d
davidchampredon <david.champredon@gmail.com>
2017-10-12 16:41:56 -0400

    minor edit

--- test.R
+++ test.R
@@ -9,8 +9,8 @@ if(length(out)==0)
     system(paste("./buildNewDB",db.name))
 
 # Pick an epidemic:
-country <- "UK"
-disease <- "measles"
+country <- "SIERRA LEONE"
+disease <- "ebola"
 
 # Retrieve the data:
 x <- get.epi.ts(db.name, country, disease, synthetic = NULL)
@@ -19,7 +19,11 @@ x <- get.epi.ts(db.name, country, disease, synthetic = NULL)
 library(ggplot2)
 x$date <- as.Date(x$reportdate)
 x2 <- subset(x, eventtype=='incidence')
-ggplot(x2) + 
+g <- ggplot(x2) + 
     geom_step(aes(x=date,y=count)) +
     ggtitle(paste(disease, country)) + 
     facet_wrap(~adminDiv1, scales='free_y')
+
+pdf('test-plot.pdf', width = 15, height = 12)
+plot(g)
+dev.off()
