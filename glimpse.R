a97b4d4be1353d850e44669aa45d73707bfcb9ee
davidchampredon <david.champredon@gmail.com>
2017-11-06 22:23:24 -0500

    minor edits

--- glimpse.R
+++ glimpse.R
@@ -16,12 +16,17 @@ dat <- q.epi %>%
   left_join(q.loc, by='location_id') %>%
   left_join(q.dis, by='disease_id')
 
+print(dat %>%
+          group_by(disease_name) %>%
+          summarize(n_datapoints = length(epievent_id),
+                    n_sources = length(unique(source))) 
+)
 x <- dat %>%
-  group_by(country, disease_name, source) %>%
-  summarize(n_datapoints = length(epievent_id)) %>%
-  mutate(source = substr(source,1,16))
+          group_by(country, disease_name, source) %>%
+          summarize(n_datapoints = length(epievent_id)) %>%
+          mutate(source = substr(source,1,16))
 print(x)
-print(paste('A total of',prettyNum(sum(x$n_datapoints),big.mark=","),'data points.'))
+print(paste(db.path, 'has a total of',prettyNum(sum(x$n_datapoints),big.mark=","),'data points.'))
 
 message('\n [glimpse end] \n')
 
