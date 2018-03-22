88ba1f84a844dc895e18277f990d0ac43aed79fb
Walt Mankowski <waltman@pobox.com>
2018-03-22 16:16:41 -0400

    replace . in variable names with camelcase

--- data/ebola-west-africa/insert_csv.R
+++ data/ebola-west-africa/insert_csv.R
@@ -10,14 +10,14 @@ source('_dbutil.R')
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 2)
     stop("usage: load_diseases.R dbfile cvsfile")
-db.name <- args[1]
-csv.name <- args[2]
+dbName <- args[1]
+csvName <- args[2]
 
 # read in CSV file
-table.csv = read_csv(csv.name)
+tableCsv = read_csv(csvName)
 
 # Connect to the database
-db = dbConnect(SQLite(), dbname = db.name)
+db = dbConnect(SQLite(), dbname = dbName)
 
 # Start a transaction
 dbBegin(db)
@@ -26,12 +26,12 @@ dbBegin(db)
 diseaseId <- getDiseaseId(db, 'ebola')
 
 # Parse out the country names from the var column
-tmp <- strsplit(x = table.csv$var, split = '--')
-table.csv$country <- trimws(sapply(tmp, '[[',2))
+tmp <- strsplit(x = tableCsv$var, split = '--')
+tableCsv$country <- trimws(sapply(tmp, '[[',2))
 
 # Incidence or death?
-table.csv$ev <- 'incidence'
-table.csv$ev[grepl('Death', table.csv$var)] <- 'death'
+tableCsv$ev <- 'incidence'
+tableCsv$ev[grepl('Death', tableCsv$var)] <- 'death'
 
 # Get source id
 sourceId <- getSourceId(db, "US CDC")
@@ -41,18 +41,18 @@ insertSql <- "
 INSERT INTO epievents (disease_id, location_id, report_date, cases, eventtype_id, source_id)
                VALUES (         ?,           ?,           ?,     ?,            ?,         ?)"
 
-for (i in 1:nrow(table.csv)) {
-    locationId <- getLocationId(db, table.csv[[i,'country']])
-    reportDate <- as.character(pull(table.csv[i, "WHO report date"]))
-    reportCases <- as.numeric(table.csv[[i, 'val']])
-    reportEv <- table.csv[[i, 'ev']]
+for (i in 1:nrow(tableCsv)) {
+    locationId <- getLocationId(db, tableCsv[[i,'country']])
+    reportDate <- as.character(pull(tableCsv[i, "WHO report date"]))
+    reportCases <- as.numeric(tableCsv[[i, 'val']])
+    reportEv <- tableCsv[[i, 'ev']]
     eventId <- getEventId(db, reportEv)
     
-    insert.rs <- dbSendStatement(db, insertSql)
-    dbBind(insert.rs, list(diseaseId, locationId, reportDate, reportCases, eventId, sourceId))
-    dbHasCompleted(insert.rs)
-    dbGetRowsAffected(insert.rs)
-    dbClearResult(insert.rs)
+    insertRs <- dbSendStatement(db, insertSql)
+    dbBind(insertRs, list(diseaseId, locationId, reportDate, reportCases, eventId, sourceId))
+    dbHasCompleted(insertRs)
+    dbGetRowsAffected(insertRs)
+    dbClearResult(insertRs)
 }
 
 # Commit the transaction
